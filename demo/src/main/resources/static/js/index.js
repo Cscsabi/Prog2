a9031cb246c7981eb8e7a5ad4dc2f9872ed10b8c
Csaba Csizmadia <71043131+Cscsabi@users.noreply.github.com>
2021-09-26 01:54:50 +0200

    Demo up

---
+++ demo/src/main/resources/static/js/index.js
@@ -0,0 +1,23 @@
+const element = document.getElementById("name");
+const target = document.getElementById("greeting");
+
+element.addEventListener("keyup", function (event){
+    const name = event.target.value;
+    target.innerHTML = "loading";
+
+    fetch('/hello?' + "name=" + name)
+        .then(function (response) {
+            return  response.text();
+        })
+        .then(function (text){
+            target.innerHTML = text;
+        }).catch(function (error) {
+            alert(error.message);
+    });
+
+    const button = document.querySelector(".btn");
+    button.addEventListener("click", function () {
+        alert("The button is clicked by " + name); //TODO
+    });
+});
+
